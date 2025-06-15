69f3be3094e9db378144595875f9b9d79081a132
Rishan-Jadva <rishanjadva@gmail.com>
2025-06-15 14:20:26 +1000

    implemented reading and writing

--- main.go
+++ main.go
@@ -8,10 +8,21 @@ import (
 func main() {
 	mux := http.NewServeMux()
 
+	fileServer := http.FileServer(http.Dir("."))
+	mux.Handle("/app/", http.StripPrefix("/app", fileServer))
+	mux.HandleFunc("/healthz", handlerReadiness)
+
 	srv := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
 	}
 
+	log.Println("Server running on http://localhost:8080")
 	log.Fatal(srv.ListenAndServe())
+}
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
